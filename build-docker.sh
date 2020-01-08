1108cfd28a7ab195bd4648dc6e551e96ee5d9522
FunkySamuel37 <masterzhiwei@gmail.com>
2020-01-08 14:39:17 +0800

    build with version

--- build-docker.sh
+++ build-docker.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+regex='refs/tags/v(.*)'
+
+VERSION_STR=latest
+if [[ $GITHUB_REF == refs/tags/v* ]]; then
+	[[ $GITHUB_REF =~ $regex ]]
+	VERSION_STR=${BASH_REMATCH[1]}
+	echo version is $VERSION_STR
+fi
+
+
 docker login --username funkyway --password $DOCKER_TOKEN
-docker build -t funkyway/github-action-test:latest .
-docker push funkyway/github-action-test:latest
+docker build -t funkyway/github-action-test:${VERSION_STR} .
+docker push funkyway/github-action-test:{VERSION_STR}
